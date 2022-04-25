435ce5799cf3b8e4b1ac9283de9be80b5fb1e475
parham <parhamhassanzade.p@gmail.com>
2022-04-25 13:32:12 +0430

    set delete and Done handler

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,24 +1,41 @@
 import React from "react";
 import { Todos } from "../model";
 import { AiOutlineEdit, AiFillDelete, AiOutlineCheck } from "react-icons/ai";
-import "./styles.css"
+import "./styles.css";
 type props = {
   todo: Todos;
   todos: Todos[];
   setTodos: React.Dispatch<React.SetStateAction<Todos[]>>;
 };
 const SingleTodo: React.FC<props> = ({ todo, todos, setTodos }) => {
+  const handleDone = (id: number) => {
+    console.log("hi");
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
   return (
     <form className="singleTodo">
-      <span className="singleTodo-Text">{todo.content}</span>
+      {todo.isDone ? (
+        (console.log("hi"), (<s className="singleTodo-Text">{todo.content}</s>))
+      ) : (
+        <span className="singleTodo-Text">{todo.content}</span>
+      )}
       <div className="icon-box">
         <span>
           <AiOutlineEdit />
         </span>
-        <span>
+        <span onClick={() => handleDelete(todo.id)}>
           <AiFillDelete />
         </span>
-        <span>
+        <span onClick={() => handleDone(todo.id)}>
           <AiOutlineCheck />
         </span>
       </div>
